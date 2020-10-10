86377b83bc1c5b29cbf940eba6ca3388b26535db
Sepkohn <50259520+sepkohn@users.noreply.github.com>
2020-10-10 18:39:34 +0200

    First Layouts

---
+++ app/src/main/java/project/bookyourtable/BookingDatasActivity.java
@@ -0,0 +1,14 @@
+package project.bookyourtable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingDatasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_datas);
+    }
+}
---
+++ app/src/main/java/project/bookyourtable/MainBookingActivity.java
@@ -0,0 +1,14 @@
+package project.bookyourtable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_booking);
+    }
+}
