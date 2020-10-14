5f281f0748916d5ce6535bab498eec31a1f97d54
QuentinBeeckmans <quentinbeeckmans@gmail.com>
2020-10-14 18:01:01 +0200

    First implementation Management table

---
+++ app/src/main/java/project/bookyourtable/TableManagementAdd.java
@@ -0,0 +1,14 @@
+package project.bookyourtable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableManagementAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_management_add);
+    }
+}
---
+++ app/src/main/java/project/bookyourtable/TableManagementModify.java
@@ -0,0 +1,14 @@
+package project.bookyourtable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableManagementModify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_management_modify);
+    }
+}
