85dbe3191bc235d06a81c2105194f8bf161c974c
QuentinBeeckmans <quentinbeeckmans@gmail.com>
2020-10-15 14:53:03 +0200

    Management table Dialog params wrong custom

--- app/src/main/java/project/bookyourtable/table/MainTableManagement.java
+++ app/src/main/java/project/bookyourtable/table/MainTableManagement.java
@@ -1,10 +1,13 @@
-package project.bookyourtable;
+package project.bookyourtable.table;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
@@ -14,25 +17,72 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainTableManagement extends AppCompatActivity {
-    private Button button;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
+import java.util.List;
+
+import project.bookyourtable.R;
+
+public class MainTableManagement extends AppCompatActivity {
+    private Button btnAdd;
+    private Button btnDel;
+    private Button btnDis;
+    private List<Integer> tables;
+
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_table_management);
 
-        button = (Button) findViewById(R.id.button5);
-        button.setOnClickListener(new View.OnClickListener() {
+        final ChipGroup chipGroup = findViewById(R.id.chipGroupTables);
+
+//        tables = chipGroup.getCheckedChipIds();
+//        for(Integer id: tables){
+//            Chip chip = chipGroup.findViewById(id);
+//        }
+
+        btnAdd = (Button) findViewById(R.id.buttonAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showAddItemDialog(MainTableManagement.this);
+                showAddItemDialog(MainTableManagement.this, R.style.AddingTableDialog);
             }
         });
+
+        btnDel = (Button) findViewById(R.id.buttonDelete);
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddItemDialog(MainTableManagement.this, R.style.AddingTableDialog);
+            }
+        });
+        btnDis = (Button) findViewById(R.id.buttonDesactivate);
+        btnDis.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View v) {
+                int idTable = chipGroup.getCheckedChipId();
+
+                Chip chip = findViewById(idTable);
+
+
+                if(chip.getChipBackgroundColor()  == (ColorStateList.valueOf(R.color.colorPrimary))){
+
+
+                }else{
+                    chip.setChipBackgroundColor(ColorStateList.valueOf(R.color.colorTest));
+                }
+
+
+            }
+        });
+
     }
 
 
-    private void showAddItemDialog(Context c) {
+    private void showAddItemDialog(Context c, int addingTableDialog) {
         final LinearLayout linearLayout = new LinearLayout(c);
 
         final EditText numberTable = new EditText(c);
@@ -73,5 +123,7 @@ public class MainTableManagement extends AppCompatActivity {
                 })
                 .create();
         dialog.show();
+
+
     }
 }
