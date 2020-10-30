3cf6e7e44045354fd4dbbdf41210c6321ee5618c
Sepkohn <50259520+Sepkohn@users.noreply.github.com>
2020-10-30 13:58:22 +0100

    Number of persons

--- app/src/main/java/project/bookyourtable/MainBookingActivity.java
+++ app/src/main/java/project/bookyourtable/MainBookingActivity.java
@@ -5,8 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.sql.Types.INTEGER;
 
 public class MainBookingActivity extends AppCompatActivity {
+
+
+
+    public MainBookingActivity(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,4 +29,32 @@ public class MainBookingActivity extends AppCompatActivity {
         Intent intent = new Intent(this, BookingDatasActivity.class);
         startActivity(intent);
     }
+
+
+    public void addPerson(View view){
+
+        EditText numberPersons = (EditText)findViewById(R.id.editTextNumber);
+        int number = Integer.parseInt(numberPersons.getText().toString().trim());
+        number = number +1;
+        displayNumbrePersons(numberPersons, number);
+    }
+
+    private void displayNumbrePersons(EditText numberPersons, int i) {
+        numberPersons.setText("" + i);
+    }
+
+    public void decreasePerson(View view){
+
+        EditText numberPersons = findViewById(R.id.editTextNumber);
+        int number = Integer.parseInt(numberPersons.getText().toString().trim());
+
+        if(number<=0){
+            displayNumbrePersons(numberPersons, 0);
+        }
+        else
+            displayNumbrePersons(numberPersons,number-1);
+
+    }
+
+    
 }
