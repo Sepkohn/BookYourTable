10b484cc8db7098679ece2b0afd524051a657043
QuentinBeeckmans <50259468+QuentinBeeckmans@users.noreply.github.com>
2020-11-10 18:23:20 +0100

    Update Booking RecycleView and tutiquanti

--- app/src/main/java/project/bookyourtable/ui/booking/BookingDatasActivity.java
+++ app/src/main/java/project/bookyourtable/ui/booking/BookingDatasActivity.java
@@ -8,17 +8,35 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import project.bookyourtable.BaseApp;
 import project.bookyourtable.R;
+import project.bookyourtable.adapter.RecyclerAdapter;
 import project.bookyourtable.database.entity.BookingEntity;
+import project.bookyourtable.database.entity.TableEntity;
 import project.bookyourtable.util.OnAsyncEventListener;
+import project.bookyourtable.util.RecyclerViewItemClickListener;
 import project.bookyourtable.viewmodel.booking.CreateBookingViewModel;
+import project.bookyourtable.viewmodel.table.TableListViewModel;
 
 public class BookingDatasActivity extends AppCompatActivity {
 
     private static final String TAG = "BookingDatasActivity";
     BookingEntity entity;
     CreateBookingViewModel createBookingViewModel;
+
+
+    //><<<<<<<<<<<<<<<<<<<<<<<<<< RAJOUTE QUENTIN
+    private TableListViewModel viewModel;
+    private List<TableEntity> tables;
+    private RecyclerAdapter<TableEntity> adapter;
+    //><<<<<<<<<<<<<<<<<<<<<<<<<< RAJOUTE QUENTIN
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +49,48 @@ public class BookingDatasActivity extends AppCompatActivity {
         CreateBookingViewModel.Factory factory = new CreateBookingViewModel.Factory((BaseApp) getApplication());
         createBookingViewModel = new ViewModelProvider(this, factory).get(CreateBookingViewModel.class);
 
+        //>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< RAJOUTE QUENTIN
+        RecyclerView recyclerView = findViewById(R.id.tableRecyclerView3);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        tables = new ArrayList<>();
+        adapter = new RecyclerAdapter<>(new RecyclerViewItemClickListener() {
+
+            public void onItemClick(View v, int position) {
+                Log.d(TAG, "clicked position:" + position);
+                Log.d(TAG, "clicked on: " + tables.get(position).getId());
+
+
+                if(tables.get(position).getAvailability()==true){
+                    tables.get(position).setAvailability(false);
+                }else{
+                    tables.get(position).setAvailability(true);
+                }
+                Log.d(TAG, "Availability:" + tables.get(position).getAvailability());
+            }
+
+            @Override
+            public void onItemLongClick(View v, int position) {
+
+            }
+        });
+
+        TableListViewModel.Factory factory2 = new TableListViewModel.Factory(
+                getApplication());
+
+        viewModel = ViewModelProviders.of(this, factory2).get(TableListViewModel.class);
+        viewModel.getOwnAccounts().observe(this, tableEntities -> {
+            if (tableEntities != null) {
+                tables = tableEntities;
+                adapter.setData(tables);
+            }
+        });
+
+        recyclerView.setAdapter(adapter); // refresh l'adaptater, toutes les vues dans le recycler sont rafraichies
+        adapter.notifyDataSetChanged();
+
+        //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     }
 
     public void validateBooking(View view){
