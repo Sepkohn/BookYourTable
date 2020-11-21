c4d98313aa03bf66c646e5f01001e37872a3234c
QuentinBeeckmans <50259468+QuentinBeeckmans@users.noreply.github.com>
2020-11-21 13:42:52 +0100

    First change to firebase

--- app/src/main/java/project/bookyourtable/database/entity/TableEntity.java
+++ app/src/main/java/project/bookyourtable/database/entity/TableEntity.java
@@ -4,6 +4,11 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Entity(tableName = "Btables")
 public class TableEntity {
     @PrimaryKey(autoGenerate = true)
@@ -26,16 +31,24 @@ public class TableEntity {
         this.availability=availability;
         this.location=location;
     }
-
+    @Exclude
     public Long getId(){return id;}
     public void setId(Long id){this.id = id; }
 
+    @Exclude
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("personNumber", personNumber);
+        result.put("availability", availability);
+        return result;
+    }
     public int getPersonNumber(){return personNumber; }
     public void setPersonNumber(int personNumber){ this.personNumber=personNumber; }
 
     public boolean getAvailability(){return availability; }
     public void setAvailability(boolean availability){ this.availability=availability; }
 
+    @Exclude
     public int getLocation(){return location; }
     public void setLocation(int location){ this.location=location; }
 
---
+++ app/src/main/java/project/bookyourtable/database/firebase/TableLiveData.java
@@ -0,0 +1,51 @@
+package project.bookyourtable.database.firebase;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import project.bookyourtable.database.entity.TableEntity;
+
+public class TableLiveData extends LiveData<TableEntity> {
+    private static final String TAG = "AccountLiveData";
+
+    private final DatabaseReference reference;
+    private final String owner;
+    private final TableLiveData.MyValueEventListener listener = new TableLiveData.MyValueEventListener();
+
+    public TableLiveData(DatabaseReference ref) {
+        reference = ref;
+        owner = ref.getParent().getParent().getKey();
+    }
+
+    @Override
+    protected void onActive() {
+        Log.d(TAG, "onActive");
+        reference.addValueEventListener(listener);
+    }
+
+    @Override
+    protected void onInactive() {
+        Log.d(TAG, "onInactive");
+    }
+
+    private class MyValueEventListener implements ValueEventListener {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            TableEntity entity = dataSnapshot.getValue(TableEntity.class);
+            entity.setId(Long.parseLong(dataSnapshot.getKey()));
+            setValue(entity);
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.e(TAG, "Can't listen to query " + reference, databaseError.toException());
+        }
+    }
+}
