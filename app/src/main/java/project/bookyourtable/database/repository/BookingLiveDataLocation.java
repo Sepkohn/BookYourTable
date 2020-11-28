5c7644ebe03a398a1c2bff1f1e0616fa43c76b39
QuentinBeeckmans <50259468+QuentinBeeckmans@users.noreply.github.com>
2020-11-28 17:35:29 +0100

    Trying to do a solution to prevent deleting table when the are booking on it

---
+++ app/src/main/java/project/bookyourtable/database/repository/BookingLiveDataLocation.java
@@ -0,0 +1,73 @@
+package project.bookyourtable.database.repository;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import project.bookyourtable.database.entity.BookingEntity;
+
+
+public class BookingLiveDataLocation extends LiveData<List<BookingEntity>> {
+    private static final String TAG = "BookingLiveDataLocation";
+    private int location;
+
+    private final DatabaseReference reference;
+
+
+    private final BookingLiveDataLocation.MyValueEventListener listener = new BookingLiveDataLocation.MyValueEventListener();
+
+
+    public BookingLiveDataLocation(DatabaseReference ref, int location) {
+        reference = ref;
+        this.location=location;
+    }
+
+
+    @Override
+    protected void onActive() {
+        Log.d(TAG, "onActive");
+        reference.addValueEventListener(listener);
+    }
+
+    @Override
+    protected void onInactive() {
+        Log.d(TAG, "onInactive");
+    }
+
+    private class MyValueEventListener implements ValueEventListener {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            setValue(toAccounts(dataSnapshot));
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.e(TAG, "Can't listen to query " + reference, databaseError.toException());
+        }
+    }
+
+    private List<BookingEntity> toAccounts(DataSnapshot snapshot) {
+        List<BookingEntity> bookings = new ArrayList<>();
+
+        for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+            for (DataSnapshot childSnapshot2 : childSnapshot.getChildren()) {
+                BookingEntity entity = childSnapshot.getValue(BookingEntity.class);
+
+                if (entity.getTableNumber().equals(String.valueOf(location))) {
+                    entity.setId(childSnapshot.getKey());
+                    bookings.add(entity);
+                }
+            }
+        }
+        return bookings;
+    }
+}
